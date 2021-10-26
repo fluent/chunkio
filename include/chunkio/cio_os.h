2d0c8223d70a140e507495a3794c5959f08028fe
Leonardo Alminana <leonardo@calyptia.com>
2021-10-26 16:12:33 -0300

    os: fixed compatibility layer issues
    Signed-off-by: Leonardo Alminana <leonardo@calyptia.com>

--- include/chunkio/cio_os.h
+++ include/chunkio/cio_os.h
@@ -20,8 +20,9 @@
 #ifndef CIO_OS_H
 #define CIO_OS_H
 #include <sys/types.h>
+#include <chunkio/chunkio_compat.h>
 
 int cio_os_isdir(const char *dir);
-int cio_os_mkpath(const char *dir, mode_t mode);
+int cio_os_mkpath(const char *dir, cio_mode_t mode);
 
 #endif
