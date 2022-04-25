25c4e7e446d77f06267b71d0dd0289f86e6c86f2
Eduardo Silva <eduardo@calyptia.com>
2022-04-25 15:54:39 -0600

    core: adjust printf formatting
    Signed-off-by: Eduardo Silva <eduardo@calyptia.com>

--- src/cio_file.c
+++ src/cio_file.c
@@ -1352,7 +1352,7 @@ char *cio_file_hash(struct cio_file *cf)
 
 void cio_file_hash_print(struct cio_file *cf)
 {
-    printf("crc cur=%ld\n", cf->crc_cur);
+    printf("crc cur=%u\n", cf->crc_cur);
     printf("%08lx\n", (long unsigned int ) cf->crc_cur);
 }
 
